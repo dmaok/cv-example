f8914ebb1940d224f2b8f0346f5cec466cfe777c
Dmytro Kovtun <d.kovtun@promorepublic.com>
2020-05-24 10:45:17 +0300

    add compression

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const compression = require('compression');
 
 const app = express();
 
+app.use(compression({
+  filter: (req, res) => {
+    if (req.headers['x-no-compression']) return false;
+    return compression.filter(req, res);
+  },
+  threshold: 0
+}));
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/cv-app'));
 
 app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/cv-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/cv-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
